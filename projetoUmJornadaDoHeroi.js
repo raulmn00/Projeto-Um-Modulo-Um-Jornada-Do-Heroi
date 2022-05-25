25a2abd4355924f6021424e2edd44e484ee1584d
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-25 15:01:45 -0300

    adicionando historia de background e perguntas para nosso heroi

--- projetoUmJornadaDoHeroi.js
+++ projetoUmJornadaDoHeroi.js
@@ -1,10 +1,17 @@
 const prompt = require('prompt-sync')();
+console.log("Após uma graduação totalmente frustrada, nosso herói encontra na programação uma nova vocação, uma paixão inusitada que poderá mudar totalmente sua vida!");
+console.log();
+console.log("Mas....para isso, ele precisará passar por todos os caminhos necessários para se tornar um bom desenvolvedor!");
+console.log();
+console.log("Você está preparado para essa jornada? Não será nada fácil!");
+console.log();
+console.log("Você deverá completar todas as missões diárias para poder atingir o seu objetivo de se tornar um MESTRE DA PROGRAMAÇÃO! Um posto que pouquíssimos conseguiram alcançar!");
 
-let pergunta1 = prompt("você conseguiu ver todos os vídeos da aula?");
-let pergunta2 = prompt("você conseguiu estudar todo o conteúdo da aula?");
-let pergunta3 = prompt("você conseguiu realizar todos os exercícios de fixação?");
-let pergunta4 = prompt("você conseguiu realizar todos os exercícios extras?");
-let pergunta5 = prompt("você conseguiu revisar todo o conteúdo da aula?");
+let pergunta1 = prompt("Me diga, jovem gafanhoto, você conseguiu ver todos os vídeos da aula? (S/N)\t");
+let pergunta2 = prompt("Me diga, jovem gafanhoto, você conseguiu estudar todo o conteúdo da aula? (S/N)\t");
+let pergunta3 = prompt("Me diga, jovem gafanhoto, você conseguiu realizar todos os exercícios de fixação? (S/N)\t");
+let pergunta4 = prompt("Me diga, jovem gafanhoto, você conseguiu realizar todos os exercícios extras? (S/N)\t");
+let pergunta5 = prompt("Me diga, jovem gafanhoto, você conseguiu revisar todo o conteúdo da aula? (S/N)\t");
 
 console.log(pergunta1);
 console.log(pergunta2);
