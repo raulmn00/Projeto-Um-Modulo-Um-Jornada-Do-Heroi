2b1a70a197c906727b363b38b7621a17f680b714
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-25 14:50:33 -0300

    Criacao do projeto na aula 02 com todas as perguntas

--- projetoUmJornadaDoHeroi.js
+++ projetoUmJornadaDoHeroi.js
@@ -1,1 +1,13 @@
-console.log("Primeiro Commit do Projeto!");
+const prompt = require('prompt-sync')();
+
+let pergunta1 = prompt("você conseguiu ver todos os vídeos da aula?");
+let pergunta2 = prompt("você conseguiu estudar todo o conteúdo da aula?");
+let pergunta3 = prompt("você conseguiu realizar todos os exercícios de fixação?");
+let pergunta4 = prompt("você conseguiu realizar todos os exercícios extras?");
+let pergunta5 = prompt("você conseguiu revisar todo o conteúdo da aula?");
+
+console.log(pergunta1);
+console.log(pergunta2);
+console.log(pergunta3);
+console.log(pergunta4);
+console.log(pergunta5);
