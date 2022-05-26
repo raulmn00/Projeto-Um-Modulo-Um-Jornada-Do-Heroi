562d3e1d0125bbf19f55ed8e42c031d8fc3ac796
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-26 19:20:15 -0300

    testando um commit pelo vs code

--- projetoUmJornadaDoHeroi.js
+++ projetoUmJornadaDoHeroi.js
@@ -47,7 +47,7 @@ if (pergunta5 == "S") {
   qtdRespostasAfirmativas += 1;
 }
 
-if ((qtdRespostasAfirmativas == 0)) {
+if (qtdRespostasAfirmativas == 0) {
   console.log(
     "Lamento herói, mas hoje você falhou miseravelmente! Tente novamente amanhã!"
   );
@@ -55,15 +55,15 @@ if ((qtdRespostasAfirmativas == 0)) {
   console.log(
     "Ok herói, você falhou por hoje, mas está no caminho certo! Continue progredindo!"
   );
-} else if ((qtdRespostasAfirmativas == 3)) {
+} else if (qtdRespostasAfirmativas == 3) {
   console.log(
     "Sinto muito herói, mas você ainda assim falhou...mas foi por tão pouco...AMANHÃ VOCÊ IRÁ CONSEGUIR! NÃO DESISTA!"
   );
-} else if ((qtdRespostasAfirmativas == 4)) {
+} else if (qtdRespostasAfirmativas == 4) {
   console.log(
     "Meus parabéns herói! Você conseguiu atingir seu objetivo e, embora tenha tido um tropeço ou outro, o importante é que você conseguiu! Lembre-se, não existe gol feio, feio é não fazer gol!"
   );
-} else if ((qtdRespostasAfirmativas == 5)) {
+} else if (qtdRespostasAfirmativas == 5) {
   console.log(
     "MEUS PARABÉNS HERÓI! VOCÊ CONQUISTOU TODOS OS SEUS DESAFIOS E ESTÁ CADA VEZ MAIS PRÓXIMO DE SE TORNAR UM MESTRE DA PROGRAMAÇÃO! CONFIE NO PROCESSO! VOCÊ CONSEGUE!!!"
   );
